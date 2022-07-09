0abe0b6ebe5f3aeb7bcd13a2c6784ecd3f9dd4b3
Nick Lemke <nicklemke@web.de>
2022-07-09 13:07:35 +0200

    setting patch size manually works now

--- nnunet_ext/training/network_training/expert_gate2/nnUNetTrainerExpertGate2.py
+++ nnunet_ext/training/network_training/expert_gate2/nnUNetTrainerExpertGate2.py
@@ -54,6 +54,74 @@ class nnUNetTrainerExpertGate2(nnUNetTrainerMultiHead):
                          save_csv, del_log, use_vit, vit_type, version, split_gpu, True, ViT_task_specific_ln, do_LSA, do_SPT,
                          network, use_param_split)
 
+    def process_plans(self, plans):
+        if self.stage is None:
+            assert len(list(plans['plans_per_stage'].keys())) == 1, \
+                "If self.stage is None then there can be only one stage in the plans file. That seems to not be the " \
+                "case. Please specify which stage of the cascade must be trained"
+            self.stage = list(plans['plans_per_stage'].keys())[0]
+        self.plans = plans
+
+        stage_plans = self.plans['plans_per_stage'][self.stage]
+        self.batch_size = stage_plans['batch_size']
+        self.net_pool_per_axis = stage_plans['num_pool_per_axis']
+        self.patch_size = np.array([50,50,50]).astype(int)#TODO
+        self.do_dummy_2D_aug = stage_plans['do_dummy_2D_data_aug']
+
+        if 'pool_op_kernel_sizes' not in stage_plans.keys():
+            assert 'num_pool_per_axis' in stage_plans.keys()
+            self.print_to_log_file("WARNING! old plans file with missing pool_op_kernel_sizes. Attempting to fix it...")
+            self.net_num_pool_op_kernel_sizes = []
+            for i in range(max(self.net_pool_per_axis)):
+                curr = []
+                for j in self.net_pool_per_axis:
+                    if (max(self.net_pool_per_axis) - j) <= i:
+                        curr.append(2)
+                    else:
+                        curr.append(1)
+                self.net_num_pool_op_kernel_sizes.append(curr)
+        else:
+            self.net_num_pool_op_kernel_sizes = stage_plans['pool_op_kernel_sizes']
+
+        if 'conv_kernel_sizes' not in stage_plans.keys():
+            self.print_to_log_file("WARNING! old plans file with missing conv_kernel_sizes. Attempting to fix it...")
+            self.net_conv_kernel_sizes = [[3] * len(self.net_pool_per_axis)] * (max(self.net_pool_per_axis) + 1)
+        else:
+            self.net_conv_kernel_sizes = stage_plans['conv_kernel_sizes']
+
+        self.pad_all_sides = None  # self.patch_size
+        self.intensity_properties = plans['dataset_properties']['intensityproperties']
+        self.normalization_schemes = plans['normalization_schemes']
+        self.base_num_features = plans['base_num_features']
+        self.num_input_channels = plans['num_modalities']
+        self.num_classes = plans['num_classes'] + 1  # background is no longer in num_classes
+        self.classes = plans['all_classes']
+        self.use_mask_for_norm = plans['use_mask_for_norm']
+        self.only_keep_largest_connected_component = plans['keep_only_largest_region']
+        self.min_region_size_per_class = plans['min_region_size_per_class']
+        self.min_size_per_class = None  # DONT USE THIS. plans['min_size_per_class']
+
+        if plans.get('transpose_forward') is None or plans.get('transpose_backward') is None:
+            print("WARNING! You seem to have data that was preprocessed with a previous version of nnU-Net. "
+                  "You should rerun preprocessing. We will proceed and assume that both transpose_foward "
+                  "and transpose_backward are [0, 1, 2]. If that is not correct then weird things will happen!")
+            plans['transpose_forward'] = [0, 1, 2]
+            plans['transpose_backward'] = [0, 1, 2]
+        self.transpose_forward = plans['transpose_forward']
+        self.transpose_backward = plans['transpose_backward']
+
+        if len(self.patch_size) == 2:
+            self.threeD = False
+        elif len(self.patch_size) == 3:
+            self.threeD = True
+        else:
+            raise RuntimeError("invalid patch size in plans file: %s" % str(self.patch_size))
+
+        if "conv_per_stage" in plans.keys():  # this ha sbeen added to the plans only recently
+            self.conv_per_stage = plans['conv_per_stage']
+        else:
+            self.conv_per_stage = 2
+
     def initialize_network(self):
         r"""Extend Initialization of Network --> Load pre-trained model (specified to setup the network).
             Optimizer and lr initialization is still the same, since only the network is different.
@@ -192,15 +260,11 @@ class nnUNetTrainerExpertGate2(nnUNetTrainerMultiHead):
 
         # -- Make the directory so there will no problems when trying to save some files -- #
         maybe_mkdir_p(self.output_folder)
-        #self.patch_size[0] = self.patch_size[1] = self.patch_size[2] = 50
         # -- Create the dataloaders again, if they are still from the last task --> do this after building -- #
         # -- The output folder, otherwise the log file will be generated in the folder from the previous task -- #
         if self.task != task:
             # -- Recreate the dataloaders for training and validation -- #
-            #print("####################### create dataloaders")
-            #print(self.patch_size)
             self.reinitialize(task)
-            #print(self.patch_size)
             # -- Now reset self.task to the current task -- #
             self.task = task
 
@@ -573,10 +637,6 @@ class nnUNetTrainerExpertGate2(nnUNetTrainerMultiHead):
         # -- Reset the internal net_num_pool_op_kernel_sizes and patch_size -- #
         print("Updating the Loss based on the provided previous trainer")
         self.net_num_pool_op_kernel_sizes = net_num_pool_op_kernel_sizes
-        print("################## set patch size")
-        #self.patch_size[0] = 50
-        #self.patch_size[1] = 50
-        #self.patch_size[2] = 50
 
         # -- Updating the loss accordingly so that the forward function will not fail due to plans changing and different task -- #
         #------------------------------------------ Partially copied from original implementation ------------------------------------------#
