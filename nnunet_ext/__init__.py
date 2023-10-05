6fd61eafb61173ce2b34d5816daefd18e1b41389
Amin Ranem <42938513+amrane99@users.noreply.github.com>
2023-10-05 14:36:45 +0200

    Update __init__.py with citation for Lifelong nnUNet

--- nnunet_ext/__init__.py
+++ nnunet_ext/__init__.py
@@ -1,2 +1,6 @@
 from __future__ import absolute_import
+print("\n\nPlease cite the following paper when using Lifelong-nnUNet:\n\nGonzález, C., Ranem, A., et al. "
+      "\"Lifelong nnU-Net: a framework for standardized medical continual learning.\" "
+      "Scientific Reports, 13(1), 9381. https://www.nature.com/articles/s41598-023-34484-2\n\n")
+print("If you have questions or suggestions, feel free to open an issue at https://github.com/MECLabTUDA/Lifelong-nnUNet\n")
 from . import *
